99ae2d279a9ed1baa09c62f5554ba2dde4971831
unknown <sunhoo5514@gmail.comexitgit config --global user.email sunhoo5514@gmail.com>
2025-11-27 10:55:59 +0900

    version4_g

---
+++ chapter02/Branch01.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class Branch01{
+
+    public Branch01(){
+        System.out.println(x: "구글 브랜치에서 작업");
+        System.out.println(x: "다음브랜치에서 작업");
+    }
+
+}
---
+++ chapter02/Branch02.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class Branch02{
+
+    public Branch02(){
+        System.out.println(x: "충돌 테스트");
+        
+    }
+
+}
